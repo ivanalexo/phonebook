a536d278333a5388486a6fcb60df7b39f9390485
Ivan Omonte <ivanalex.oc@gmail.com>
2023-07-06 01:01:40 -0400

    add cors middleware

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cors = require('cors');
 const morgan = require('morgan');
 const Person = require('./models/phonebook');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors())
 app.use(express.static('build'))
 
 morgan.token('body', function(req, res) {
